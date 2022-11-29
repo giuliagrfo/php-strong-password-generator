ae0514ee2f1bb14600aa4a34a906801ff42197d7
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-29 14:56:48 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
